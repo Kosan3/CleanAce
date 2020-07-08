604dc212011254ab392eb5a2b8efa9fb7577f218
vagrant <vagrant@localhost.localdomain>
2020-07-08 05:21:14 +0000

    deviseのroute設定終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # デバイス
+  devise_for :users, skip: :all
+  devise_scope :user do
+    # session
+    get '/sign_in', to: 'users/sessions#new'
+    post '/sign_in', to: 'users/sessions#create'
+    # registration
+    get '/sign_up', to: 'users/registrations#new'
+    post '/sign_up', to: 'users/registrations#create'
+    # password
+    get '/password', to: 'users/passwords#new'
+    post '/password', to: 'users/passwords#create'
+  end
+
+  # ユーザー
+  resource :users, only: [:show, :edit, :update]
+  namespace :admin do
+    resources :users, only: [:index, :show, :edit, :update]
+  end
 end
