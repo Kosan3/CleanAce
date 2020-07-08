46f8791ced5ff72bf0d7b1d321445ea28f049961
vagrant <vagrant@localhost.localdomain>
2020-07-08 08:21:19 +0000

    routingとcontroller記述完了,favoriteモデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,52 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users, only: [:index, :show, :edit, :update]
   end
+
+  # 商品
+  resources :products, only: [:index, :show] do
+    get :top, on: :collection
+    # いいね
+    resource :favorites, only: [:create, :destroy]
+  end
+  namespace :admin do
+    resources :products
+  end
+
+  # タイプ
+  namespace :admin do
+    resources :types, only: [:index, :create, :edit, :update]
+  end
+
+  # カート
+  resources :carts, only: [:index, :create, :update, :destroy] do
+    delete :destroy_all
+  end
+
+  # 注文
+  resources :orders, only: [:index, :new, :create, :show] do
+    post :confirmation, on: :new
+    get :complete, on: :collection
+  end
+  namespace :admin do
+    resources :orders, only: [:index, :show]
+  end
+
+  # 試合結果
+  resources :results, only: [:index, :show]
+  namespace :admin do
+    resources :results, only: [:new, :create, :edit, :update, :destroy]
+  end
+
+  # メンバー
+  resources :members, only: [:index, :show]
+  namespace :admin do
+    resources :members, only: [:new, :create, :edit, :update, :destroy]
+  end
+
+  # 募集
+  resources :entries, only: [:new, :create]
+  namespace :admin do
+    resources :entries, only: [:index, :show]
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_152640) do
+ActiveRecord::Schema.define(version: 2020_07_08_081852) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2020_07_07_152640) do
     t.string "position"
     t.string "age"
     t.text "other"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
