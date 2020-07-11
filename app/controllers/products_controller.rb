0c4b11e107ae494b99d412b89102aa50c5921a66
vagrant <vagrant@localhost.localdomain>
2020-07-11 16:25:03 +0000

    top画面のレイアウト作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,11 @@
 class ProductsController < ApplicationController
   def index
-    
   end
 
   def show
-    
+    @type = Type.find(params[:id])
+    @products = Product.where(type_id: params[:id])
+    @images = ProductImage.where(type_id: params[:id])
   end
 
   def top
