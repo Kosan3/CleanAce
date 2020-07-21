a8f645f82d6d1aec03c6d4124d3cab6cbd33b24d
vagrant <vagrant@localhost.localdomain>
2020-07-21 13:44:51 +0000

    ユーザー側完成

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,18 @@
 class EntriesController < ApplicationController
+
   def new
+    @entry = Entry.new
   end
 
   def create
+    @entry = Entry.new(entry_params)
+    @entry.save
+    redirect_to root_path
+  end
+
+  private
+  def entry_params
+    params.require(:entry).permit(:last_name,:first_name,:kana_last_name,:kana_first_name,:postal_code,
+      :address,:email,:phone_number,:position,:other,:age)
   end
 end
--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,6 @@
 class ResultsController < ApplicationController
   def index
-    @results = Result.all
+    @results = Result.all.order(created_at: :desc)
   end
 
   def show
