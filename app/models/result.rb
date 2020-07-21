bdc3910a7d591fe6ef07735d2672a9053bebc779
vagrant <vagrant@localhost.localdomain>
2020-07-21 03:29:48 +0000

    result機能制作

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,2 +1,30 @@
 class Result < ApplicationRecord
+
+  def first_team_total_score
+    ining_score(self.top_first) + ining_score(self.top_second) + ining_score(self.top_third) + ining_score(self.top_fourth) + ining_score(self.top_fifth) + ining_score(self.top_sixth) + ining_score(self.top_seventh)
+  end
+
+  def second_team_total_score
+    ining_score(self.bottom_first) + ining_score(self.bottom_second) + ining_score(self.bottom_third) + ining_score(self.bottom_fourth) + ining_score(self.bottom_fifth) + ining_score(self.bottom_sixth) + ining_score(self.bottom_seventh)
+  end
+
+  def result_name
+    if self.first_team.include?("クリーンエース")
+      if first_team_total_score > second_team_total_score
+        "WIN!!"
+      elsif first_team_total_score < second_team_total_score
+        "LOSE.."
+      else
+        "DROW"
+      end
+    elsif self.second_team.include?("クリーンエース")
+      if second_team_total_score > first_team_total_score
+        "WIN!!"
+      elsif second_team_total_score < first_team_total_score
+        "LOSE.."
+      else
+        "DROW"
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_011748) do
+ActiveRecord::Schema.define(version: 2020_07_21_003640) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
@@ -127,6 +127,9 @@ ActiveRecord::Schema.define(version: 2020_07_10_011748) do
     t.integer "bottom_seventh"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_team"
+    t.string "second_team"
+    t.text "description"
   end
 
   create_table "ships", force: :cascade do |t|
