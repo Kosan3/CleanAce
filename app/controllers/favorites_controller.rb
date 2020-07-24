cb7ce3d07e82b4d423c828cfabb12973dea6cab8
vagrant <vagrant@localhost.localdomain>
2020-07-24 15:45:11 +0000

    非同期通信作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,13 @@
 class FavoritesController < ApplicationController
   def create
-    type = Type.find(params[:product_id])
-    favorite = current_user.favorites.new(type_id: type.id)
+    @type = Type.find(params[:product_id])
+    favorite = current_user.favorites.new(type_id: @type.id)
     favorite.save
-    redirect_to product_path(type)
   end
 
   def destroy
-    type = Type.find(params[:product_id])
-    favorite = current_user.favorites.find_by(type_id: type.id)
+    @type = Type.find(params[:product_id])
+    favorite = current_user.favorites.find_by(type_id: @type.id)
     favorite.destroy
-    redirect_to product_path(type)
   end
 end
