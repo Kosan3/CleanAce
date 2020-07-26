ef37345a923804d4f5b7ac069aace199cbadd031
vagrant <vagrant@localhost.localdomain>
2020-07-26 01:34:38 +0000

    to

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,22 +8,32 @@ class CartsController < ApplicationController
 
   def create
     type = Type.find(params[:product_id])
-    select_product = type.squeeze_product(params)
-    if @user.has_in_cart(select_product)
-      in_cart = @user.has_in_cart(select_product)
-      new_quantity = in_cart.quantity + cart_params[:quantity].to_i
-      in_cart.update(quantity: new_quantity)
+    if select_product = type.squeeze_product(params)
+      if @user.has_in_cart(select_product)
+        in_cart = @user.has_in_cart(select_product)
+        new_quantity = in_cart.quantity + cart_params[:quantity].to_i
+        in_cart.update(quantity: new_quantity)
+      else
+        cart = Cart.new(product_id: select_product.id, user_id: current_user.id, quantity: cart_params[:quantity])
+        cart.save
+      end
+      redirect_to carts_path
     else
-      cart = Cart.new(product_id: select_product.id, user_id: current_user.id, quantity: cart_params[:quantity])
-      cart.save
+      redirect_to product_path(type), alert: "サイズとカラーを選択してください"
     end
-    redirect_to carts_path
   end
 
   def update
+    cart_product = @user.carts.find_by(product_id: params[:id])
+    update_quantity = cart_params[:quantity].to_i
+    cart_product.update(quantity: update_quantity)
+    redirect_to carts_path
   end
 
   def destroy
+    cart_product = @user.carts.find_by(product_id: params[:id])
+    cart_product.destroy
+    redirect_to carts_path
   end
 
   def destroy_all
