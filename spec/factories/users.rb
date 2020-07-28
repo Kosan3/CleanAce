ddfa5cca7c007f30d422716ed1292a4e7264fd43
vagrant <vagrant@localhost.localdomain>
2020-07-28 09:15:34 +0000

    rspec,capybara,factoriesの設定完了

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    last_name {'テスト_姓'}
+    first_name {'テスト_名'}
+    kana_last_name {'テストセイ'}
+    kana_first_name {'テストメイ'}
+    email {'example@com'}
+    password {'password'}
+    postal_code {'1111111'}
+    address {'千葉県浦安市舞浜1-1'}
+    phone_number {'09012341234'}
+  end
+end
