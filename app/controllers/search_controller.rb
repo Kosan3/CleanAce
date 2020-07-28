f1cf11c4559aee96c296e7ac1213339abb38d137
vagrant <vagrant@localhost.localdomain>
2020-07-28 10:26:10 +0000

    search-viewを修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,6 @@ class SearchController < ApplicationController
   def search
     content = params[:search][:content]
     @q = Product.where('product_name LIKE ?', "%#{content}%").ransack(params[:q])
-    @types = @q.result(distinct: true)
+    @products = @q.result(distinct: true)
   end
 end
